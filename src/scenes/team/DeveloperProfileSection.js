79c90f89633e5684e54911d7c9b6fc6fb6943381
marcusngwj <marcusngwj@msn.com>
2018-04-05 20:21:46 +0800

    Add social media buttons

--- src/scenes/team/DeveloperProfileSection.js
+++ src/scenes/team/DeveloperProfileSection.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import { DeveloperProfilePic } from 'commons/image_objects/DeveloperProfilePic';
+import GitHubButton from 'commons/components/buttons/GitHubButton';
+import LinkedInButton from 'commons/components/buttons/LinkedInButton';
 
 export default class DeveloperProfileSection extends Component {
   // constructor(props) {
@@ -11,6 +13,8 @@ export default class DeveloperProfileSection extends Component {
     return (
       <div>
         <img src={DeveloperProfilePic.AKANKSHITA}/>
+        <GitHubButton url="https://www.google.com"/>
+        <LinkedInButton />
         <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
       </div>
     );
