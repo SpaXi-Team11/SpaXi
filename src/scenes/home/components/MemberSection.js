14c35c678a3a59850c525b71bf7d4240b2831418
marcusngwj <marcusngwj@msn.com>
2018-04-09 23:14:45 +0800

    update members

--- src/scenes/home/components/MemberSection.js
+++ src/scenes/home/components/MemberSection.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import MemberCell from './MemberCell';
+import { DeveloperDisplayPic } from 'commons/image_objects/DeveloperDisplayPic';
+
 export default class MemberSection extends Component {
   // constructor(props) {
   //   super(props);
@@ -7,8 +10,14 @@ export default class MemberSection extends Component {
 
   render() {
     return (
-      <div>
-        <p>The SpaXians</p>
+      <div className="member-section">
+        <h1>The SpaXians</h1>
+        <MemberCell image={DeveloperDisplayPic.AKANKSHITA}/>
+        <MemberCell image={DeveloperDisplayPic.MARCUS}/>
+        <MemberCell image={DeveloperDisplayPic.PING_CHAN}/>
+        <MemberCell image={DeveloperDisplayPic.WEN_HAO}/>
+        <MemberCell image={DeveloperDisplayPic.ZHANG_YING}/>
+        <MemberCell image={DeveloperDisplayPic.ZHUANG_LEI}/>
       </div>
     );
   }
