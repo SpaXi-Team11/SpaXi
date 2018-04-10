2c95410926476fa25e10e563d0bde762e6a9df3d
marcusngwj <marcusngwj@msn.com>
2018-04-10 23:56:38 +0800

    Update advisors

--- src/scenes/team/components/Advisors.js
+++ src/scenes/team/components/Advisors.js
@@ -4,6 +4,7 @@ import 'scenes/team/styles/style.css';
 import zhaoJin from 'assets/members/advisor_display_picture/ZhaoJin.png';
 import cristina from 'assets/members/advisor_display_picture/Cristina.png';
 import thilina from 'assets/members/advisor_display_picture/Thilina.png';
+import rayyan from 'assets/members/advisor_display_picture/RayYan.png';
 
 import AdvisorProfileCell from './AdvisorProfileCell';
 
@@ -16,10 +17,11 @@ export default class Advisors extends Component {
     return (
       <div className="advisors">
         <h1 className="theme__section-header">Advisors</h1>
-        <div className="display-cell">
-          <AdvisorProfileCell image={zhaoJin} name="Jin Zhao" designature="Lecturer" />
-          <AdvisorProfileCell image={cristina} name="Cristina Carbunaru" designature="Lecturer" />
+        <div className="display-section">
+          <AdvisorProfileCell image={zhaoJin} name="Dr. Zhao Jin" designature="Lecturer" />
+          <AdvisorProfileCell image={cristina} name="Dr. Cristina Carbunaru" designature="Lecturer" />
           <AdvisorProfileCell image={thilina} name="Thilina" designature="Teaching Assistant" />
+          <AdvisorProfileCell image={rayyan} name="Ang Ray Yan" designature="Technical Support" />
         </div>
       </div>
     );
