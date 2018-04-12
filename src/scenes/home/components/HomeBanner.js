79067689c0e1ade5b98848720c5e99ada84af1d0
marcusngwj <marcusngwj@msn.com>
2018-04-13 00:19:50 +0800

    update home

--- src/scenes/home/components/HomeBanner.js
+++ src/scenes/home/components/HomeBanner.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import 'scenes/home/styles/style.css';
+import softwareBox from 'assets/brand/spaxi_box.png';
+import ViewOnGitHubButton from 'commons/components/buttons/ViewOnGitHubButton';
 
 export default class HomeBanner extends Component {
   // constructor(props) {
@@ -10,7 +12,19 @@ export default class HomeBanner extends Component {
   render() {
     return (
       <div class="home-banner">
-      
+        <div class="left">
+          <img src={softwareBox} />
+        </div>
+        <div class="right">
+          <div class="text">
+            <p class="desc-top">A reliant companion</p>
+            <p class="desc-middle">STATIC PROGRAM ANALYZER</p>
+            <p class="desc-bottom">simple solution for <span>SIMPLE</span> program</p>
+          </div>
+          <div class="button-area">
+            <ViewOnGitHubButton />{/*to put a dl button instead*/}
+          </div>
+        </div>
       </div>
     );
   }
